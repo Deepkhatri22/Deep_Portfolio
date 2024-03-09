5845935d146f9299847db5c5c31352a15f2bf695
Deep Khatri <95419605+Deepkhatri22@users.noreply.github.com>
2024-03-09 19:53:24 +0530

    Resume Button

--- src/Component/HomeSection.jsx
+++ src/Component/HomeSection.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import './homesection.css'
 
 function HomeSection() {
+
     return (
         <>
             <div className="sectionoutermost">
                 <div className="left">
-                    <p>If you are Looking for a web developer or need help with your project then feel free to contact me at <span>deepkhatri934@gmail.com</span></p>
+                    <p>If you are Looking for a web developer or need help with your project then feel free to contact me at <span>deepkhatri934@gmail.com</span>
+                    </p>
+                    <a
+                        href="/Resume.pdf" // Assuming your resume file is named 'Resume.pdf' in the public folder
+                        className='text-white bg-purple-500 rounded-md w-40 mt-5 p-2 text-center block'
+                        download="Resume.pdf"
+                    >
+                        RESUME
+                    </a>
                 </div>
+
                 <div className="right">
                     <h1 className='text-white font-[550]'>
                         Let's<br />
